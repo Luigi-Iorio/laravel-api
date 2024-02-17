69362b8c3730d0fec6a833520acdd544e79c3add
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-17 19:37:10 +0100

    refactor: seeder ProjectsTableSeeder

---
+++ config/projects_db.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    [
+        'title' => 'Spotify Clone',
+        'slug' => '',
+        'stack' => 'VS CODE',
+        'description' => 'L\'obiettivo principale di questo progetto era creare una web app con un layout responsivo che assicurasse che il contenuto della pagina rimanesse all\'interno della viewport.',
+        'type_id' => 5,
+    ],
+    [
+        'title' => 'Discord Clone',
+        'slug' => '',
+        'stack' => 'VS CODE',
+        'description' => 'L\'obiettivo principale di questo progetto è riprodurre un layout basato su uno screenshot fornito, cercando di essere il più fedeli possibile al design originale.',
+        'type_id' => 5,
+    ],
+    [
+        'title' => 'Whatsapp Clone',
+        'slug' => '',
+        'stack' => 'VS CODE',
+        'description' => 'Questa repository presenta un clone di WhatsApp Web sviluppato utilizzando i linguaggi HTML, CSS, JavaScript e VueJs.',
+        'type_id' => 5,
+    ],
+    [
+        'title' => 'Netflix Clone',
+        'slug' => '',
+        'stack' => 'VS CODE',
+        'description' => 'L\'obbiettivo di questo progetto è replicare la logica di siti di streaming famosi e di cercare e visualizzare informazioni dettagliate su film e serie TV utilizzando l\'API di The Movie Database (TMDb).',
+        'type_id' => 5,
+    ]
+];
--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProjectsTableSeeder extends Seeder
@@ -14,26 +16,12 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $projects = [
-            [
-                'title' => 'Spotify Clone',
-                'slug' => '',
-                'stack' => 'html - css',
-                'description' => 'L\'obiettivo principale di questo progetto era creare una web app con un layout responsivo che assicurasse che il contenuto della pagina rimanesse all\'interno della viewport.',
-            ],
-            [
-                'title' => 'Discord Clone',
-                'slug' => '',
-                'stack' => 'html - css',
-                'description' => 'L\'obiettivo principale di questo progetto è riprodurre un layout basato su uno screenshot fornito, cercando di essere il più fedeli possibile al design originale.',
-            ],
-            [
-                'title' => 'Whatsapp Clone',
-                'slug' => '',
-                'stack' => 'html - css - js',
-                'description' => 'L\'obiettivo era quello di replicare Whatsapp-Web',
-            ]
-        ];
+        $projects = config('projects_db');
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0'); // disabilita chiavi esterne
+        DB::table('projects')->truncate(); // elimina i dati nella tabella projects
+        DB::table('project_technology')->truncate(); // elimina le associazioni
+        DB::statement('SET FOREIGN_KEY_CHECKS=1'); // riabilita chiavi esterne
 
         foreach ($projects as $project) {
             $newProject = new Project();
@@ -41,6 +29,7 @@ class ProjectsTableSeeder extends Seeder
             $newProject->slug = Str::of($newProject->title)->slug('-');
             $newProject->stack = $project['stack'];
             $newProject->description = $project['description'];
+            $newProject->type_id = $project['type_id'];
             $newProject->save();
         }
     }
